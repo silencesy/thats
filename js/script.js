d0122c24e92e2d9f88ca6472531606dd5464eb21
R <6106786451@qq.com>
2017-08-02 18:39:08 +0800

    123

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 $(function(){
 	// 消除移动端300ms的click延迟
-		window.addEventListener( "load", function() {
-			FastClick.attach( document.body );
-		}, false );
+	window.addEventListener( "load", function() {
+		FastClick.attach( document.body );
+	}, false );
+	// 头部下拉菜单js
+	$('#search-module-list li a').on('click',function(){
+		$('#navsearchmodule').text($(this).text());
 	});
+});
